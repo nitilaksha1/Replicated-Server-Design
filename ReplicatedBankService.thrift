5bdcf29484f121755d387645fda2cc229b642042
Nitilaksha Halakatti <halak004@umn.edu>
2017-03-28 16:05:59 -0500

    Commiting Thrift file

--- ReplicatedBankService.thrift
+++ ReplicatedBankService.thrift
@@ -1,14 +1,19 @@
+struct TimeStamp {
+    1: required i32 timestamp;
+    2: required i32 serverid;
+}
+
 service ReplicatedBankService {
 
-   string 	multi_deposit 			(1:i32 uID, 2:i32 amount, 3:i32 timestamp, 4:i32 serverid),
-   i32 		multi_getBalance 		(1:i32 uID, 2:i32 timestamp, 3:i32 serverid),
-   string 	multi_transfer 			(1:i32 srcuID, 2:i32 targuID, 3:i32 amount, 4:i32 timestamp, 5:i32 serverid),
-   void 	multi_deposit_ack		(1:i32 reqTimeStamp, 2:i32 serverid),
-   void 	multi_getBalance_ack 	(1:i32 reqTimeStamp, 2:i32 serverid),
-   void 	multi_transfer_ack 		(1:string requestID, 2:i32 serverid, 3:i32 timestamp),
-   oneway void multi_transfer_server(1:i32 srcuID, 2:i32 targuID, 3:i32 amount, 4:i32 timestamp, 5:i32 serverid, 6:string requestID)
+   string 	multi_deposit 			(1:i32 uID, 2:i32 amount, 3:TimeStamp timestamp, 4:i32 serverid),
+   i32 		multi_getBalance 		(1:i32 uID, 2:TimeStamp timestamp, 3:i32 serverid),
+   string 	multi_transfer 			(1:i32 srcuID, 2:i32 targuID, 3:i32 amount, 4:TimeStamp timestamp, 5:i32 serverid),
+   void 	multi_deposit_ack		(1:TimeStamp reqTimeStamp, 2:i32 serverid),
+   void 	multi_getBalance_ack 	(1:TimeStamp reqTimeStamp, 2:i32 serverid),
+   void 	multi_transfer_ack 		(1:string requestID, 2:i32 serverid, 3:TimeStamp timestamp),
+   oneway void multi_transfer_server(1:i32 srcuID, 2:i32 targuID, 3:i32 amount, 4:TimeStamp timestamp, 5:i32 serverid, 6:string requestID)
    void halt()
-   void stop_execution(1:i32 timestamp)
+   void stop_execution(1:TimeStamp timestamp)
 
 
    
